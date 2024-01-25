27232b1ce4e07193a428d5aad5dcf60106051d9b
avinash <avinash.verma@gmail.com>
2024-01-25 19:40:54 +0530

    added posts collection

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,12 +1,12 @@
-import path from 'path'
+import path from "path";
 
-import { payloadCloud } from '@payloadcms/plugin-cloud'
-import { mongooseAdapter } from '@payloadcms/db-mongodb'
-import { webpackBundler } from '@payloadcms/bundler-webpack'
-import { buildConfig } from 'payload/config'
-import { lexicalEditor } from '@payloadcms/richtext-lexical'
+import { payloadCloud } from "@payloadcms/plugin-cloud";
+import { mongooseAdapter } from "@payloadcms/db-mongodb";
+import { webpackBundler } from "@payloadcms/bundler-webpack";
+import { buildConfig } from "payload/config";
+import { lexicalEditor } from "@payloadcms/richtext-lexical";
 
-import Users from './collections/Users'
+import { Users, Posts } from "./collections";
 
 export default buildConfig({
   admin: {
@@ -14,15 +14,15 @@ export default buildConfig({
     bundler: webpackBundler(),
   },
   editor: lexicalEditor({}),
-  collections: [Users],
+  collections: [Users, Posts],
   typescript: {
-    outputFile: path.resolve(__dirname, 'payload-types.ts'),
+    outputFile: path.resolve(__dirname, "payload-types.ts"),
   },
   graphQL: {
-    schemaOutputFile: path.resolve(__dirname, 'generated-schema.graphql'),
+    schemaOutputFile: path.resolve(__dirname, "generated-schema.graphql"),
   },
   plugins: [payloadCloud()],
   db: mongooseAdapter({
     url: process.env.DATABASE_URI,
   }),
-})
+});
